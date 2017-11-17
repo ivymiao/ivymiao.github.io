080ca72957a41822c930ac4749b12f841d9d3569
于轩一 <xuanyi@yuxuanyideMacBook-Air.local>
2017-11-17 23:36:48 +0800

    Site updated: 2017-11-17 23:36:47

---
+++ js/particles.oli.js
@@ -0,0 +1,124 @@
+$(function () {
+
+    particlesJS("particles-oli-wrapper", {
+        "particles": {
+            "number": {
+                "value": 150,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                // "image": {
+                //     "src": "img/github.svg",
+                //     "width": 100,
+                //     "height": 100
+                // }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 30,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.6,
+                "width": 0.5
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "bounce",
+                "bounce": true,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "repulse": {
+                    "distance": 100,
+                    "duration": 0.4
+                    }
+            },
+        },
+        "retina_detect": true
+    });
+
+    var $particle = $('#particles-oli-wrapper').css({ display: 'none'}).removeClass('hide');
+    var $navH1 = $('.index-wrapper header h1, .index-wrapper header nav').hide();
+
+    var $header = $('.index-wrapper .hide')
+                    .removeClass('hide')
+                    .css({
+                        height: 0
+                    })
+                    .animate({
+                        height: 120
+                    }, 800, function() {
+                        $particle.fadeIn(600);
+                        $navH1.fadeIn(600);
+                    });
+
+    $('.head-nav__link').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        
+        $particle.fadeOut(600);
+        $navH1.fadeOut(100);
+
+        $header.animate({
+            height: 0
+        }, 800, function() {
+            window.location.href = $this.attr('href');
+        });
+    });
+
+});
